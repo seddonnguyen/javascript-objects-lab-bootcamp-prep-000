a3a9853387e3c136080efb1e28db71237006ec0e
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-13 00:47:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
 }
